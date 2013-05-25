0034c0d324a9545db79efd437d593f43e5c61fd3
antass <anna.krentowicz@gmail.com>
2013-05-25 17:29:30 -0400

    added comments

--- publish.sh
+++ publish.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 ## Commit changes to get correct timestamp
+echo 'Committing changes to git' 
 git add .
 git commit -am "$1"
 git push
 
 ## Compile the site
-jekyll --server 4003
+echo 'Building site'
+jekyll
 
 ## Copy site to repository for github hosting
+echo 'Uploading site to Dreamhost'
 rsync -avz --checksum /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com/labnotebook  
 echo "Site updated and uploaded to Dreamhost"
 
