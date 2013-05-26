a9688762ba6c4c4519edfbff94b4ce83ed0082c9
antass <anna.krentowicz@gmail.com>
2013-05-26 13:49:44 -0400

    update eln.aniatassinari.com

--- publish.sh
+++ publish.sh
@@ -12,6 +12,7 @@ jekyll
 
 ## Copy site to repository for github hosting
 echo 'Uploading site to Dreamhost'
-rsync -avz --checksum --delete /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:aniatassinari.com/labnotebook  
+#rsync -avz --checksum --delete /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:aniatassinari.com/labnotebook  
+rsync -avz --checksum --delete /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:eln.aniatassinari.com
 echo "Site updated and uploaded to Dreamhost"
 
