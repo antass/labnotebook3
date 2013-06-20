12b71daf3fbec01a0d399e83b52feafae8537277
antass <anna.krentowicz@gmail.com>
2013-06-19 23:06:54 -0400

    try vaio

--- publish.sh
+++ publish.sh
@@ -8,12 +8,11 @@ git push
 
 ## Compile the site
 echo 'Building site'
-jekyll build
+jekyll
 
 ## Copy site to repository for github hosting
 echo 'Uploading site to Dreamhost'
 #rsync -avz --checksum --delete /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:aniatassinari.com/labnotebook  
 rsync -avz --checksum --delete /home/ania/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:eln.aniatassinari.com
-rsync -avz --checksum --delete /Users/antass/Dropbox/labnotebook/_site/ antass@quicksilver.dreamhost.com:eln.aniatassinari.com
 echo "Site updated and uploaded to Dreamhost"
 
